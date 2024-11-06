783394eb6f94fed390f72139bee7fa46bf2e7e98
Krishan Apshaya <117499008+Apshaya@users.noreply.github.com>
2024-11-06 18:41:22 +0530

    add css and ejs files

---
+++ app.js
@@ -0,0 +1,33 @@
+require('dotenv').config();
+
+const express =  require('express');
+const expressLayout = require('express-ejs-layouts');
+
+const app = express();
+const port = 5000 || process.env.PORT;
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+//static files
+app.use (express.static('public'));
+
+//Templating engine
+app.use(expressLayout);
+app.set('layout', './layouts/main');
+app.set('view engine', 'ejs');
+
+//Home
+app.get('/', (req,res) => {
+
+    const locals = {
+        title: 'Nodejs',
+        description: 'Free Nodejs User Management System'
+
+    }
+    res.render('index', locals);
+});
+
+app.listen(port, ()=> {
+    console.log(`App listeing on port ${port}`)
+});
