54cece86f72fded19703f76a747695bab4ed6865
Krishan Apshaya <117499008+Apshaya@users.noreply.github.com>
2024-11-07 11:16:15 +0530

    ejs file creation

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ require('dotenv').config();
 
 const express =  require('express');
 const expressLayout = require('express-ejs-layouts');
+const connectDB = require('./server/config/db');
 
 const app = express();
 const port = 5000 || process.env.PORT;
+
+//Connect to Database
+connectDB();
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
@@ -17,15 +21,12 @@ app.use(expressLayout);
 app.set('layout', './layouts/main');
 app.set('view engine', 'ejs');
 
-//Home
-app.get('/', (req,res) => {
+//Routes
+app.use('/', require('./server/routes/customer'))
 
-    const locals = {
-        title: 'Nodejs',
-        description: 'Free Nodejs User Management System'
-
-    }
-    res.render('index', locals);
+//handle 404
+app.get('*',(req,res) => {
+    res.status(404).render('404');
 });
 
 app.listen(port, ()=> {
